7ecac96646ecee5d45e74b440fb4e2b636928fce
Mohamed OUADA <mouada@0952284s.peda>
2023-09-11 17:00:50 +0200

    cour 18

---
+++ src/Controller/TestController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    #[Route('/test', name: 'app_test')]
+    public function index(): Response
+    {
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'TestController',
+        ]);
+        /*return $this->json(['username' => 'jane.doe']);*/
+    }
+
+    #[Route('/test/{id}', name: 'test_id', methods: ["GET"], requirements: ['id' => '\d+'])]
+    public function test_id($id): Response
+    {
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'TestController',
+            'controller_id'=>$id,
+        ]);
+    }
+}
